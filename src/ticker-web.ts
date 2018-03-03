d1a14069f72d6fdf662a9cdbc60054575b2ba6e8
Luis Farzati <lufohs@gmail.com>
2018-03-03 20:05:48 +0100

    initial commit

---
+++ src/ticker-web.ts
@@ -0,0 +1,17 @@
+import * as express from "express"
+import * as path from "path"
+
+import { parseEnv } from "./env"
+
+const isoNow = () => new Date().toISOString()
+const writeOut = (m: string) => process.stdout.write(`${isoNow()} ${m}\n`)
+
+const env = parseEnv()
+
+const app = express()
+
+app.use(express.static("public"))
+
+app.listen(env.TICKER_UI_PORT, () => {
+  writeOut(`Starting ticker UI at port ${env.TICKER_WS_PORT}`)
+})
